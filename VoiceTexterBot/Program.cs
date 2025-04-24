14cf8984be525912ba023d942f3dcbb33e98f7a3
vladkop2025 <vladkop@mail.ru>
2025-04-24 21:46:20 +0300

    master

--- VoiceTexterBot/Program.cs
+++ VoiceTexterBot/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Профессия C#-разработчик Язык C# Модуль 11. Telegram-бот
+namespace VoiceTexterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
