65d65d968bd6aadc9578f0ab81017dc308638399
vladkop2025 <vladkop@mail.ru>
2025-04-25 12:14:37 +0300

    unit3

--- VoiceTexterBot/Controllers/InlineKeyboardController.cs
+++ VoiceTexterBot/Controllers/InlineKeyboardController.cs
@@ -6,23 +6,61 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoiceTexterBot.Services;
+
+//namespace VoiceTexterBot.Controllers
+//{
+//    class InlineKeyboardController
+//    {
+//        private readonly ITelegramBotClient _telegramClient;
+
+//        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+//        {
+//            _telegramClient = telegramBotClient;
+//        }
+
+//        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+//        {
+//            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+//            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+//        }
+//    }
+//}
 
 namespace VoiceTexterBot.Controllers
 {
-    class InlineKeyboardController
+    public class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
 
-        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
         {
             _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
         }
 
         public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+            if (callbackQuery?.Data == null)
+                return;
 
-            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+            // Обновление пользовательской сессии новыми данными
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+            // Генерим информационное сообщение
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => " Русский",
+                "en" => " Английский",
+                _ => String.Empty
+            };
+
+            // Отправляем в ответ уведомление о выборе
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
         }
     }
 }
--- VoiceTexterBot/Controllers/TextMessageController.cs
+++ VoiceTexterBot/Controllers/TextMessageController.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace VoiceTexterBot.Controllers
 {
@@ -20,8 +22,27 @@ namespace VoiceTexterBot.Controllers
 
         public async Task Handle(Message message, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
-            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+            switch (message.Text)
+            {
+                case "/start":
+
+                    // Объект, представляющий кнопки
+                    var buttons = new List<InlineKeyboardButton[]>();
+                    buttons.Add(new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData($" Русский" , $"ru"),
+                        InlineKeyboardButton.WithCallbackData($" English" , $"en")
+                    });
+
+                    // передаем кнопки вместе с сообщением (параметр ReplyMarkup)
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот превращает аудио в текст.</b> {Environment.NewLine}" +
+                        $"{Environment.NewLine}Можно записать сообщение и переслать другу, если лень печатать.{Environment.NewLine}", cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
+
+                    break;
+                default:
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Отправьте аудио для превращения в текст.", cancellationToken: ct);
+                    break;
+            }
         }
     }
 }
